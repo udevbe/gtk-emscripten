GNOME/gtk

Add GskPathMeasure
GskPathMeasure is an auxiliary object for
measuring path lengths.

gsk/gskcontourprivate.h
@@ -67,14 +67,17 @@ void                    gsk_contour_get_start_end               (const GskContou
                                                                  graphene_point_t       *end);
 int                     gsk_contour_get_winding                 (const GskContour       *self,
                                                                  const graphene_point_t *point);
+
 gboolean                gsk_contour_get_closest_point           (const GskContour       *self,
                                                                  const graphene_point_t *point,
                                                                  float                   threshold,
                                                                  GskPathPoint           *result,
                                                                  float                  *out_dist);
+
 void                    gsk_contour_get_position                (const GskContour       *self,
                                                                  GskPathPoint           *point,
                                                                  graphene_point_t       *pos);
+
 void                    gsk_contour_get_tangent                 (const GskContour       *self,
                                                                  GskPathPoint           *point,
                                                                  GskPathDirection        direction,
@@ -82,5 +85,23 @@ void                    gsk_contour_get_tangent                 (const GskContou
 float                   gsk_contour_get_curvature               (const GskContour       *self,
                                                                  GskPathPoint           *point,
                                                                  graphene_point_t       *center);
+gpointer                gsk_contour_init_measure                (const GskContour       *self,
+                                                                 float                   tolerance,
+                                                                 float                  *out_length);
+void                    gsk_contour_free_measure                (const GskContour       *self,
+                                                                 gpointer                data);
+void                    gsk_contour_add_segment                 (const GskContour       *self,
+                                                                 GskPathBuilder         *builder,
+                                                                 gpointer                measure_data,
+                                                                 gboolean                emit_move_to,
+                                                                 float                   start,
+                                                                 float                   end);
+void                    gsk_contour_get_point                   (const GskContour       *self,
+                                                                 gpointer                measure_data,
+                                                                 float                   offset,
+                                                                 GskPathPoint           *result);
+float                   gsk_contour_get_distance                (const GskContour       *self,
+                                                                 GskPathPoint           *point,
+                                                                 gpointer                measure_data);
 
 G_END_DECLS

udevbe/gtk-emscripten

gsk/gskcontourprivate.h
@@ -57,14 +57,17 @@ void                    gsk_contour_get_start_end               (const GskContou
                                                                  graphene_point_t       *end);
 int                     gsk_contour_get_winding                 (const GskContour       *self,
                                                                  const graphene_point_t *point);
+
 gboolean                gsk_contour_get_closest_point           (const GskContour       *self,
                                                                  const graphene_point_t *point,
                                                                  float                   threshold,
                                                                  GskRealPathPoint       *result,
                                                                  float                  *out_dist);
+
 void                    gsk_contour_get_position                (const GskContour       *self,
                                                                  GskRealPathPoint       *point,
                                                                  graphene_point_t       *pos);
+
 void                    gsk_contour_get_tangent                 (const GskContour       *self,
                                                                  GskRealPathPoint       *point,
                                                                  GskPathDirection        direction,
@@ -72,5 +75,23 @@ void                    gsk_contour_get_tangent                 (const GskContou
 float                   gsk_contour_get_curvature               (const GskContour       *self,
                                                                  GskRealPathPoint       *point,
                                                                  graphene_point_t       *center);
+gpointer                gsk_contour_init_measure                (const GskContour       *self,
+                                                                 float                   tolerance,
+                                                                 float                  *out_length);
+void                    gsk_contour_free_measure                (const GskContour       *self,
+                                                                 gpointer                data);
+void                    gsk_contour_add_segment                 (const GskContour       *self,
+                                                                 GskPathBuilder         *builder,
+                                                                 gpointer                measure_data,
+                                                                 gboolean                emit_move_to,
+                                                                 float                   start,
+                                                                 float                   end);
+void                    gsk_contour_get_point                   (const GskContour       *self,
+                                                                 gpointer                measure_data,
+                                                                 float                   offset,
+                                                                 GskRealPathPoint       *result);
+float                   gsk_contour_get_distance                (const GskContour       *self,
+                                                                 GskRealPathPoint       *point,
+                                                                 gpointer                measure_data);
 
 G_END_DECLS