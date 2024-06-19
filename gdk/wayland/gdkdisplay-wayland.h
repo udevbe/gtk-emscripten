a06c412f5fdcd0d0adb9655a956d39c6dfdd9d32
Carlos Garnacho <carlosg@gnome.org>
2024-06-19 13:20:38 +0200

    wayland: Get/initialize a xdg_session_v1 object
    Add the internal API to let the upper layers define a session
    ID (e.g. saved from previous runs), and retrieve the session ID
    eventually used (e.g. in case the session is created from scratch).

--- gdk/wayland/gdkdisplay-wayland.h
+++ gdk/wayland/gdkdisplay-wayland.h
@@ -42,6 +42,7 @@
 #include <gdk/wayland/presentation-time-client-protocol.h>
 #include <gdk/wayland/single-pixel-buffer-v1-client-protocol.h>
 #include <gdk/wayland/xdg-dialog-v1-client-protocol.h>
+#include <gdk/wayland/xdg-session-management-v1-client-protocol.h>
 
 #include <glib.h>
 #include <gdk/gdkkeys.h>
@@ -124,6 +125,8 @@ struct _GdkWaylandDisplay
   struct wp_viewporter *viewporter;
   struct wp_presentation *presentation;
   struct wp_single_pixel_buffer_manager_v1 *single_pixel_buffer;
+  struct xdg_session_manager_v1 *xdg_session_manager;
+  struct xdg_session_v1 *xdg_session;
   GdkWaylandColor *color;
 
   GList *async_roundtrips;
@@ -152,6 +155,8 @@ struct _GdkWaylandDisplay
   struct xkb_context *xkb_context;
 
   GListStore *monitors;
+
+  char *session_id;
 
   gint64 last_bell_time_ms;
 };
