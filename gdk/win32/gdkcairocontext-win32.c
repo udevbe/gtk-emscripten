RobloxEnderman/gtk

GdkWin32: Clean up gdk_win32_surface_handle_queued_move_resize()
Make that function accept a GdkSurface, not a GdkDrawContext, to make things
clearer to people.

gdk/win32/gdkcairocontext-win32.c
@@ -66,7 +66,7 @@ gdk_win32_cairo_context_begin_frame (GdkDrawContext *draw_context,
   surface = gdk_draw_context_get_surface (draw_context);
   scale = gdk_surface_get_scale_factor (surface);
 
-  queued_window_rect = gdk_win32_surface_handle_queued_move_resize (draw_context);
+  queued_window_rect = gdk_win32_surface_handle_queued_move_resize (surface);
 
   width = queued_window_rect.right - queued_window_rect.left;
   height = queued_window_rect.bottom - queued_window_rect.top;

udevbe/gtk-emscripten

gdk/win32/gdkcairocontext-win32.c
@@ -68,7 +68,7 @@ gdk_win32_cairo_context_begin_frame (GdkDrawContext  *draw_context,
   surface = gdk_draw_context_get_surface (draw_context);
   scale = gdk_surface_get_scale_factor (surface);
 
-  queued_hwnd_rect = gdk_win32_surface_handle_queued_move_resize (draw_context);
+  queued_hwnd_rect = gdk_win32_surface_handle_queued_move_resize (surface);
 
   width = queued_hwnd_rect.right - queued_hwnd_rect.left;
   height = queued_hwnd_rect.bottom - queued_hwnd_rect.top;